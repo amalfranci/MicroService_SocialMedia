eb17f84e15047f4807fbce2168aa288cf4c7f98f
amalfranci <amalfrancis744@gmail.com>
2025-02-20 15:59:14 +0530

    formatted

--- media-service/src/eventHandler/mediaEventHandler.js
+++ media-service/src/eventHandler/mediaEventHandler.js
@@ -1,35 +1,27 @@
-const Media = require("../models/media.js")
-const { deleteMediaFromCloudnary } = require("../utils/cloudnary")
-const logger = require("../utils/logger")
+const Media = require("../models/media.js");
+const { deleteMediaFromCloudnary } = require("../utils/cloudnary");
+const logger = require("../utils/logger");
 
-const handlePostDeleted =  async(event)=>{
-    console.log(event,"eventeventevent")
-    const {postId,mediaIds} = event
-    console.log("postId===============>",postId ,"mediaIDS================>",mediaIds)
+const handlePostDeleted = async (event) => {
+  const { postId, mediaIds } = event;
 
-    try{
-       
-        const mediaToDelete = await Media.find({ publicId: { $in: mediaIds } });
-  
-        // Validate results
-        if (!mediaToDelete.length) {
-          console.log("Amal ERORRRRRRRRRRRRRRRRRRR=>",mediaToDelete)
-        }
-        console.log("mediaToDelete=============>",mediaToDelete)
+  try {
+    const mediaToDelete = await Media.find({ publicId: { $in: mediaIds } });
 
-        for (const media of mediaToDelete){
-            await deleteMediaFromCloudnary(media.publicId)
-            await Media.findByIdAndDelete(media._id)
-
-            logger.info(`Deleted media ${media._id} `)
-        }
-        logger.info(`Process delection is completed ${postId}`)
-
+    // Validate results
+    if (!mediaToDelete.length) {
     }
-    catch(error)
-    {
-        logger.error(error,'Error occur when delete a file')
-    }
-}
 
-module.exports={handlePostDeleted}
+    for (const media of mediaToDelete) {
+      await deleteMediaFromCloudnary(media.publicId);
+      await Media.findByIdAndDelete(media._id);
+
+      logger.info(`Deleted media ${media._id} `);
+    }
+    logger.info(`Process delection is completed ${postId}`);
+  } catch (error) {
+    logger.error(error, "Error occur when delete a file");
+  }
+};
+
+module.exports = { handlePostDeleted };
